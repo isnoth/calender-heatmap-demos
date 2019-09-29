eeab7454f5a1c438955df97b41c03fb22768c305
yaolin <yao.lin@nokia.com>
2019-09-29 17:11:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CalHeatMap from 'cal-heatmap'
+import 'cal-heatmap/cal-heatmap.css'
+import dayjs from 'dayjs'
+
+const secondsOfDate = (date) => dayjs(date).startOf('day')/1000
+
+const Inc = (datas, date, num=1) => {
+    const key =  secondsOfDate(date)
+    if (datas[key]) {
+        return Object.assign({}, datas, {[key]: datas[key]+1})
+    } else {
+        return Object.assign({}, datas, {[key]:1 })
+    }
+}
+
+class HeatMap extends Component {
+    constructor(props) {
+        super(props)
+        this.cal = new CalHeatMap();
+        const datas = this.props
+        this.state = {
+            datas
+        }
+    }
+    componentDidMount() {
+        this.cal.init({
+            itemSelector: "#cal-heatmap",
+            domain: "month",
+            subDomain: "day",
+            cellSize: 20, subDomainTextFormat: "%d",
+            range: 3,
+            start: new Date(new Date().getTime() - 1000*60*60*24/*hour*/ * 60/*day*/),
+            displayLegend: false,
+            data: this.state.datas,
+            onClick: (date, nb) => {
+                this.setState({
+                    datas: Inc(this.state.datas, date, 1)
+                })
+                this.cal.update(this.state.datas)
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.cal.destroy()
+    }
+
+    render() {
+        return <p id="cal-heatmap"> world</p>
+    }
+}
+
+class Item extends Component {
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        hello world
+      <HeatMap/>
     </div>
   );
 }
